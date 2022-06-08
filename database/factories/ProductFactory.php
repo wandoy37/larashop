01b44ea0803d935a39df96e4a759eda63453e192
wandoy37 <riswandi035@gmail.com>
2022-06-08 14:40:39 +0800

    Make Mode Product, Migration, Seeder & UserSeeder

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->word(),
+            'description' => $this->faker->text(120),
+            'price' => $this->faker->numberBetween(10000, 100000),
+            'image' => ''
+        ];
+    }
+}
